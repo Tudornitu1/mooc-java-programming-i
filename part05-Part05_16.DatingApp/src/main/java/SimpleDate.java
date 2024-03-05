236bfcd6956ffbd9cb67c97bb23ec7d02a72c4e6
Tudornitu1 <tudornitu@yahoo.com>
2024-03-05 15:08:43 +0200

    new work

--- part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
+++ part05-Part05_16.DatingApp/src/main/java/SimpleDate.java
@@ -26,11 +26,45 @@ public class SimpleDate {
         }
 
         if (this.year == compared.year && this.month == compared.month &&
-                 this.day < compared.day) {
+                this.day < compared.day) {
             return true;
         }
 
         return false;
+    }
+
+    public void advance() {
+        if (this.day < 30) {
+            this.day++;
+        } else if (this.month < 12) {
+            this.month++;
+            this.day = 1;
+        } else {
+            this.year++;
+            this.month = 1;
+            this.day = 1;
+        }
+    }
+
+    public void advance(int howManyDays) {
+        if (this.day + howManyDays <= 30) {
+            this.day += howManyDays;
+        } else if (this.month + howManyDays / 30 + 1 < 12) {
+            this.month += howManyDays / 30+1;
+            this.day = this.day+howManyDays % 30-30;
+        } else {
+            this.year += howManyDays / 360 + 1;
+            this.month = howManyDays / 12 + 1;
+            this.day = howManyDays % 30;
+        }
+
+    }
+
+    public SimpleDate afterNumberOfDays(int days){
+        SimpleDate newDate = new SimpleDate(day, month, year);
+        newDate.advance(days);
+        return newDate;
+        
     }
 
 }
--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -1,8 +1,8 @@
 
 public class Money {
 
-    private final int euros;
-    private final int cents;
+    private int euros;
+    private int cents;
 
     public Money(int euros, int cents) {
 
@@ -32,4 +32,41 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition){
+        Money newMoney=new Money(euros, cents);
+        if (newMoney.cents+addition.cents > 99) {
+            newMoney.euros = newMoney.euros + (newMoney.cents+addition.cents) / 100+addition.euros;
+            newMoney.cents = (newMoney.cents+addition.cents) % 100;
+        } else {
+            newMoney.euros+=addition.euros;
+            newMoney.cents+=addition.cents;
+        }
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared){
+        if(this.euros<compared.euros){
+            return true;
+        } else if(this.cents<compared.cents&&this.euros<=compared.euros){
+            return true;
+        }
+        else return false;
+    }
+
+    public Money minus(Money decreaser){
+        Money newMoney=new Money(euros, cents);
+        if(newMoney.cents<decreaser.cents){
+            newMoney.cents=(100+newMoney.cents-decreaser.cents);
+            newMoney.euros=newMoney.euros-decreaser.euros-1;
+        } else {
+            newMoney.euros=newMoney.euros-decreaser.euros;
+            newMoney.cents=newMoney.cents-decreaser.cents;
+        }
+        if(newMoney.euros<0){
+            newMoney.cents=0;
+            newMoney.euros=0;
+        }
+
+        return newMoney;
+    }
 }
